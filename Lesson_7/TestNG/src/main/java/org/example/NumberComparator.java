cb75b742d3bff9b976123dba755a2912c1603d0c
Яков Бугаков <159473376+SyURcH@users.noreply.github.com>
2025-10-20 16:21:48 +0300

    Delete Lesson_7/TestNG directory

--- Lesson_7/TestNG/src/main/java/org/example/NumberComparator.java
+++
@@ -1,5 +0,0 @@
-public class NumberComparator {
-    public static int compare(int a, int b) {
-        return Integer.compare(a, b);
-    }
-}
--- Lesson_7/TestNG/src/main/java/org/example/TriangleArea.java
+++
@@ -1,9 +0,0 @@
-public class TriangleArea {
-    public static double calculateArea(double a, double b, double c) {
-        if (a <= 0 || b <= 0 || c <= 0) throw new IllegalArgumentException("Sides must be positive");
-        if (a + b <= c || a + c <= b || b + c <= a) throw new IllegalArgumentException("Invalid triangle");
-
-        double s = (a + b + c) / 2;
-        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
-    }
-}
